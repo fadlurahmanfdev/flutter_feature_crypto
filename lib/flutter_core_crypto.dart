d76659099389ae2042f82191d0c9d2f46489b363
fadlurahmanf <tffajari@gmail.com>
2024-04-25 18:47:20 +0700

    fix(library): export impl class

--- lib/flutter_core_crypto.dart
+++ lib/flutter_core_crypto.dart
@@ -1,5 +1,7 @@
 library library_core_crypto;
 
+export 'data/dto/exception/core_crypto_exception.dart';
+export 'data/dto/model/crypto_key.dart';
 export 'data/repositories/crypto_aes_repository.dart';
 export 'data/repositories/crypto_aes_repository_impl.dart';
 export 'data/repositories/crypto_rsa_repository.dart';
